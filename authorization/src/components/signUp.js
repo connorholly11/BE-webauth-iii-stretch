1a68f9cb6c1214d09c6928055999b202c3af540e
Connor Holly <connorholly11@gmail.com>
2019-10-16 17:15:25 -0400

    able to add users

--- authorization/src/components/signUp.js
+++ authorization/src/components/signUp.js
@@ -1,21 +1,50 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const SignUp = () => {
-  const [form, setForm] = useState({ username: "", password: "" });
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+    department: ""
+  });
 
   const changehandler = e => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  const submitForm = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+      .post("http://localhost:5000/api/register", form)
+      .then(res => {
+        console.log(res);
+      });
+  };
+
   return (
     <div>
-      <form>
+      <h1>Sign up form</h1>
+      <form onSubmit={submitForm}>
         <input
-          type="text"
+          name="username"
           placeholder="username"
-          value={form.user}
+          value={form.username}
           onChange={changehandler}
         />
+        <input
+          name="password"
+          placeholder="password"
+          value={form.password}
+          onChange={changehandler}
+        />
+        <input
+          name="department"
+          placeholder="department"
+          value={form.department}
+          onChange={changehandler}
+        />
+        <button type="submit">Sign Up!</button>
       </form>
     </div>
   );
